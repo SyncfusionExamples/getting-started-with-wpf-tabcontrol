543c58f38267238197e27386a12ac003af1ab1d6
Sivakumar <sivakumarr@syncfusion.com>
2019-10-17 12:42:56 +0530

    WPF TabControl Example committed.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabControlDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_NewButtonClick(object sender, EventArgs e)
+        {
+            //Write code to add new tab.
+        }
+    }
+}
